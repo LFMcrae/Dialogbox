98f687b235a9a4facaca37f0e65b0fbd730c2639
LFMcrae <659457679@qq.com>
2017-09-15 15:59:52 +0800

    Initial commit

---
+++ app/src/main/java/com/example/roony/dialogbox/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.roony.dialogbox;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final View v2 = getLayoutInflater().inflate(R.layout.help, null);
+        final View v1 = getLayoutInflater().inflate(R.layout.loginview, null);
+        final EditText userId = (EditText) v1.findViewById(R.id.editText_UserID);
+        final EditText password = (EditText) v1.findViewById(R.id.editText_Password);
+        Button button2 = (Button) findViewById(R.id.button_help);
+        Button button1 = (Button) findViewById(R.id.button_login);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                LayoutInflater inflater = getLayoutInflater();
+
+                builder.setView(v1).setTitle("Login");
+
+                builder.setPositiveButton("login",new DialogInterface.OnClickListener(){
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int id){
+                        if(userId.getText().toString().equals("abc") && password.getText().toString().equals("123")){
+                            Toast.makeText(MainActivity.this,"登录成功",Toast.LENGTH_LONG).show();
+                        }
+                        else{
+                            Toast.makeText(MainActivity.this,"登录失败",Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
+                builder.setNegativeButton("cancel",new DialogInterface.OnClickListener(){
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int id){
+                        Toast.makeText(MainActivity.this,"用户按下了取消按钮",Toast.LENGTH_LONG).show();
+                    }
+                });
+                builder.show();
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                LayoutInflater inflater = getLayoutInflater();
+
+                builder.setView(v2).setMessage("Select your hero,OKOK");
+                builder.show();
+            }
+        });
+
+    }
+}
